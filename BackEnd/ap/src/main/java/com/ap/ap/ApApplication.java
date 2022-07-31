87c01826105effae578b279b4932966190a96281
florensg <floo.g1096@gmail.com>
2022-07-31 09:26:48 -0300

    .

--- BackEnd/ap/src/main/java/com/ap/ap/ApApplication.java
+++ BackEnd/ap/src/main/java/com/ap/ap/ApApplication.java
@@ -20,12 +20,12 @@ public class ApApplication {
             CorsConfiguration corsConfiguration = new CorsConfiguration();
             corsConfiguration.setAllowCredentials(true);
             corsConfiguration.setAllowedOrigins(Arrays.asList("http://localhost:4200"));
-            corsConfiguration.setAllowedHeaders(Arrays.asList("Origin","Acccess-Control-Allow-Origin","Content-Type",
-                    "Accept","Authorization", "Origin, Accept","X-Requested-With",
-                    "Acces-Control-Request-Method","Access-Control-Request-Headers"));
-            corsConfiguration.setExposedHeaders(Arrays.asList("Origin","Content-Type","Accept","Authorization",
-                    "Access-Control-Allow-Origin","Access-Control-Allow-Origin","Access-Control-Allow-Credentials"));
-            corsConfiguration.setAllowedMethods(Arrays.asList("GET","POST","PUT","DELETE","OPTIONS"));
+            corsConfiguration.setAllowedHeaders(Arrays.asList("Origin", "Acccess-Control-Allow-Origin", "Content-Type",
+                     "Accept", "Authorization", "Origin, Accept", "X-Requested-With",
+                    "Acces-Control-Request-Method", "Access-Control-Request-Headers"));
+            corsConfiguration.setExposedHeaders(Arrays.asList("Origin", "Content-Type", "Accept", "Authorization",
+                     "Access-Control-Allow-Origin", "Access-Control-Allow-Origin", "Access-Control-Allow-Credentials"));
+            corsConfiguration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
             UrlBasedCorsConfigurationSource urlBasedCorsConfigurationSource = new UrlBasedCorsConfigurationSource();
             urlBasedCorsConfigurationSource.registerCorsConfiguration("/**", corsConfiguration);
             return new CorsFilter(urlBasedCorsConfigurationSource);
