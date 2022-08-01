7e8f33011c37d1360af1c8b8194c65ba7651e78d
florensg <floo.g1096@gmail.com>
2022-07-31 22:50:03 -0300

    .

---
+++ BackEnd/ap/src/main/java/com/ap/ap/AplicationSecurityConfig.java
@@ -0,0 +1,15 @@
+package com.ap.ap;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled=true)
+public class AplicationSecurityConfig extends WebSecurityConfigurerAdapter{
+
+    
+}
